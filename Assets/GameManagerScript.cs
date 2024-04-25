a21750126143a4e67f839aab1e869f18bbe5ccbe
arima-naoto <k023g1010@g.neec.ac.jp>
2024-04-25 17:08:40 +0900

    EN1_01_01_移動(配列操作)_演習

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -12,7 +12,6 @@ public class GameManagerScript : MonoBehaviour
     void Start()
     {
         map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
-        //追加。文字列の宣言と初期化
       
     }
 
@@ -20,8 +19,11 @@ public class GameManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int playerIndex = -1;
+
+        //右キーを押している場合
         if (Input.GetKeyDown(KeyCode.RightArrow)){
-            int playerIndex = -1;
+        
             //要素数はmap.Lenghtで取得
             for (int i = 0; i < map.Length; i++)
             {
@@ -49,6 +51,37 @@ public class GameManagerScript : MonoBehaviour
             }
             Debug.Log(debugText);
 
+        }
+
+        //左キーを押している場合
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //要素数はmap.Lenghtで取得
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            //playerIndex + 1のインデックスのものと交換するので、
+            //playerIndex - 1よりさらに小さいインデックスの時
+            //のみの交換処理を行う
+            if (playerIndex > 0)
+            {
+                map[playerIndex - 1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            string debugText2 = "";
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText2 += map[i].ToString() + ",";
+            }
+            Debug.Log(debugText2);
         }
     }
     

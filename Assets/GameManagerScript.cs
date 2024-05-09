b2b56947fb27581c5049e14b6a3011f7a491eade
arima-naoto <k023g1010@g.neec.ac.jp>
2024-05-09 17:07:36 +0900

    GameObjectのインスタンスを作成

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-   
+    public GameObject playerPrefab;
+
+    //レベルデザイン用の配列
+    int[,] map; 
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        map = new int[,]
+        {
+            { 0,0,0,0,0 },
+            { 0,0,1,0,0 },
+            { 0,0,0,0,0 },
+        };
+
+        for(int y = 0; y < map.GetLength(0); y++)
+        {
+            for(int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[x,y] == 1)
+                {
+                    GameObject instance = Instantiate(
+                        playerPrefab, 
+                        new Vector3(x, map.GetLength(0) - y,0), 
+                        Quaternion.identity
+                        );
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame

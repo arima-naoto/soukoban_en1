2cd50a2fa551028695893d8534284770be901ab0
arima-naoto <k023g1010@g.neec.ac.jp>
2024-05-17 12:08:47 +0900

    パーティクルのスクリプトを追加

---
+++ Assets/Particle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+
+    private float lifeTime;
+
+    private float leftLifeTime;
+
+    private Vector3 velocity;
+
+    private Vector3 defaultScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTime = 0.3f;
+
+        leftLifeTime = lifeTime;
+
+        defaultScale = transform.localScale;
+
+        float maxVelocity = 5;
+
+        velocity = new Vector3
+            (Random.Range(-maxVelocity, maxVelocity),Random.Range(-maxVelocity, maxVelocity),0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftLifeTime += Time.deltaTime;
+
+        transform.position += velocity * Time.deltaTime;
+
+        transform.localScale = Vector3.Lerp(Vector3.zero, defaultScale, leftLifeTime / lifeTime);
+
+        if(leftLifeTime <= 0) { Destroy(gameObject); }
+    }
+}
